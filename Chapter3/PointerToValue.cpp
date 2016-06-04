f2028ff298676d6a2c03b2b7a15b580d996bdcff
Dakota Hawkins <dakotahawkins0110@gmail.com>
2016-06-03 19:18:50 -0700

    Added template function to find value via pointer of first value.

--- Chapter3/PointerToValue.cpp
+++ Chapter3/PointerToValue.cpp
@@ -12,12 +12,24 @@
 #include <iostream>
 using namespace std;
 
-template <typename element> element* find_value(const vector<element> &vec, element value);
-
-template <typename element> element* find_value(const vector<element> &vec, element value) {
+template <typename element> element* find_value(vector<element> &vec, element value);
+template <typename element> element* find_value(vector<element> &vec, element value) {
   for (int i = 0; i < vec.size(); i++) {
     if (vec[i] == value) {
       return &vec[i];
+    }
+  }
+  return 0;
+}
+
+template <typename T> T* find_value(T *array, int size, T &value);
+template <typename T> T* find_value(T *array, int size, T value) {
+  if (! array || size < 1) {
+    return 0;
+  }
+  for (int i = 0; i < size; i++) {
+    if (array[i] == value) {
+      return &array[i];
     }
   }
   return 0;
@@ -27,12 +39,14 @@ int main() {
   int int_array[5] = {1,2,3,4,5};
   string str_array[5] = {"Hello", ",", "how", "are", "you?"};
   float float_array[5] = {0.123, 4.5, 10.1, 5.7, 10};
-  const vector<int> int_vector(int_array, int_array + 5);
-  const vector<string> str_vector(str_array, str_array + 5);
-  const vector<float> float_vector(float_array, float_array + 5);
-  cout << "Pointer of '3' in 'int_vector': " << find_value(int_vector, 3);
-  cout << "Pointer of 'Hello' in 'str_vector': " << find_value(str_vector, string("HI"));
+  vector<int> int_vector(int_array, int_array + 5);
+  vector<string> str_vector(str_array, str_array + 5);
+  vector<float> float_vector(float_array, float_array + 5);
+  cout << "Pointer of '3' in 'int_vector': " << find_value(int_vector, 3) << endl;
+  cout << "Pointer of 'Hello' in 'str_vector': " << find_value(str_vector, string("Hello")) << endl;
   float test = 11.1;
-  cout << "Pointer of '10.1' in '5.7' in 'float_vector': " << find_value(float_vector, test);
+  cout << "Pointer of '11.1' in 'float_vector': " << find_value(float_vector, test) << endl;
+  float first_value = float_array[1];
+  cout << "Pointer of 4.5 in 'float_array': " << find_value(&first_value, int(5), float(4.5)) << endl;
   return 0;
 }
