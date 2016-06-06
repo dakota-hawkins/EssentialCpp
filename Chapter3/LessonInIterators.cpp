211dc86efddf9fa6d8cf69515e3aa191ef77aa14
Hawkins, Dakota Y <dakota.hawkins@pnnl.gov>
2016-06-06 13:50:02 -0700

    Generalized 'find' algorithm for lists, vectors, and arrays.

--- Chapter3/LessonInIterators.cpp
+++ Chapter3/LessonInIterators.cpp
@@ -6,4 +6,57 @@
 //  Copyright © 2016 Hawkins, Dakota Y. All rights reserved.
 //
 
-#include "LessonInIterators.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <list>
+using namespace std;
+
+template <typename element> void display(const vector<element> &vector, ostream &os);
+template <typename element> void display(const vector<element> &vector, ostream &os) {
+  typename::vector<element>::const_iterator iter = vector.begin();
+  typename::vector<element>::const_iterator end_it = vector.end();
+  for (; iter != end_it; ++iter) {
+    os << *iter << ' ';
+  }
+  os << endl;
+}
+
+template <typename iterator_type, typename element> iterator_type find_value(iterator_type first, iterator_type last, const element &value);
+template <typename iterator_type, typename element> iterator_type find_value(iterator_type first, iterator_type last, const element &value) {
+  for (; first != last; ++first) {
+    if (value == *first) {
+      return first;
+    }
+  }
+  return last;
+}
+
+
+int main() {
+  const int array_size = 8;
+  int int_array[array_size] = {1,1,2,3,5,8,13,21};
+  vector<int> int_vec(int_array, int_array + array_size);
+  list<int> int_list(int_array, int_array + array_size);
+  // Array instance
+  int * int_pointer = find_value(int_array, int_array+array_size, 5);
+  if (int_pointer != (int_array + array_size)) {
+    cout << "Found! " << int_pointer << endl;
+  }
+  // Vector instance
+  vector<int>::iterator vec_iter;
+  vec_iter = find_value(int_vec.begin(), int_vec.end(), 1024);
+  if (vec_iter != int_vec.end()) {
+    cout << "Found! " << &vec_iter << endl;
+  } else {
+    cout << "1024 not found in integer vector." << endl;
+  }
+  // List instance
+  list<int>::iterator list_iter;
+  list_iter = find_value(int_list.begin(), int_list.end(), 13);
+  if (list_iter != int_list.end()) {
+    cout << "13 held at: " << &list_iter << endl;
+  }
+  return 0;
+}
--- Chapter3/LessonInIterators.hpp
+++ Chapter3/LessonInIterators.hpp
@@ -11,4 +11,5 @@
 
 #include <stdio.h>
 
+
 #endif /* LessonInIterators_hpp */
