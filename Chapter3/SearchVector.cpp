5e789039233f1a0474aa7cea7f41e6b85e85a4bd
Hawkins, Dakota Y <dakota.hawkins@pnnl.gov>
2016-06-06 13:50:36 -0700

    find value over a vector using iterators.

--- Chapter3/SearchVector.cpp
+++ Chapter3/SearchVector.cpp
@@ -7,3 +7,26 @@
 //
 
 #include "SearchVector.hpp"
+
+template <typename element> element* use_sentinel(element *first_address, element *sentinel, element &value) {
+  if (! first_address || ! sentinel) {
+    return 0;
+  }
+  for(; first_address != sentinel; ++first_address) {
+    if (*(first_address) == value) {
+      return first_address;
+    }
+  }
+  return 0;
+}
+
+int main() {
+  int integer_array[5] = {1,2,3,4,5};
+  vector<int> integer_vector(integer_array, integer_array + 5);
+  vector<string> string_vector;
+  cout << "Beginning: " << container_start(integer_vector) << "\n Ending: " << container_end(integer_vector) << endl;
+  cout << "Found 3 at: " << use_sentinel(container_start(integer_vector), container_end(integer_vector), integer_array[3]) << endl;
+  string simple_string = "Nothing here!";
+  cout << "What happens with a null vector: " << use_sentinel(container_start(string_vector), container_end(string_vector), simple_string) << endl;
+  return 0;
+}
--- Chapter3/SearchVector.hpp
+++ Chapter3/SearchVector.hpp
@@ -10,5 +10,21 @@
 #define SearchVector_hpp
 
 #include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+template <typename element> element* use_sentinel(element *first_address, element *sentinel, element &value);
+
+template <typename element> inline element* container_start(vector<element> &vec);
+template <typename element> inline element* container_start(vector<element> &vec) {
+  return (vec.empty() ? 0 : &vec[0]);
+}
+
+template <typename element> inline element* container_end(vector<element> &vec);
+template <typename element> inline element* container_end(vector<element> &vec) {
+  return (vec.empty() ? 0 : &(vec[vec.size()]) + 1);
+}
 
 #endif /* SearchVector_hpp */
