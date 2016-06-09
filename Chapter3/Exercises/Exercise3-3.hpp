a3e0d35ce12b3e714b46d50f2c11189cff9d8903
Hawkins, Dakota Y <dakota.hawkins@pnnl.gov>
2016-06-09 10:54:15 -0700

    Initial push for Exercise 3.3 from Essential C++.

---
+++ Chapter3/Exercises/Exercise3-3.hpp
@@ -0,0 +1,13 @@
+#ifndef Exercise3-3_hpp
+#define Exercise3-3_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+#endif /* Exercise3-2_hpp */
