965583afd8b9e2c5bddec9e4a8d81ca1380f7de8
Hawkins, Dakota Y <dakota.hawkins@pnnl.gov>
2016-06-15 17:16:24 -0700

    Introduction the Triangular Iterator class and such.

--- Chapter4/Exercises/Triangular.cpp
+++ Chapter4/Exercises/Triangular.cpp
@@ -1,5 +1,7 @@
 #include "Triangular.hpp"
 
+vector<int> Triangular::_elements;
+
 Triangular::Triangular() : _name("Triangular") {
   // Default constructor
   _length = 1;
@@ -25,3 +27,64 @@ Triangular::Triangular(const Triangular &rhs)
     _beginning_position(rhs._beginning_position),
     _next(rhs._beginning_position - 1)
 {}
+
+int Triangular::element(int position) const {
+  return _elements[position - 1];
+}
+
+Triangular& Triangular::copy(const Triangular &rhs) {
+  if (this != &rhs) {
+    _length = rhs._length;
+    _beginning_position = rhs._beginning_position;
+    _next = rhs._beginning_position;
+  }
+  return *this;
+}
+
+// Static Member functions
+
+bool Triangular::is_element(int value) {
+  if (! _elements.size() || _elements[_elements.size() - 1 < value]) {
+    generate_elements_to_value(value);
+  }
+  vector<int>::iterator found_it;
+  vector<int>::iterator end_it = _elements.end();
+
+  found_it = find(_elements.begin(), end_it, value);
+  return (found_it != end_it);
+}
+
+void Triangular::generate_elements(int length) {
+  if (length < 0 || length > _max_elements) {
+    cerr << "Error generate_elements(): desired length outside window (0, "
+    << _max_elements << "]" <<  endl;
+  }
+  if (_elements.size() < length) {
+    int i = _elements.size() ? _elements.size() + 1 : 1;
+    for ( ; i <= length - 1; ++i) {
+      _elements.push_back(i*(i + 1)/2);
+    }
+  }
+}
+
+void Triangular::display(int length, int beginning_position, ostream &os) {
+  for (int i = beginning_position - 1; i < length; ++i) {
+    os << _elements[i] << ((i + 1) % 10 ? " " : "\n");
+  }
+}
+
+void Triangular::generate_elements_to_value(int value) {
+  int i = _elements.size();
+  if (! i) {
+    _elements.push_back(i);
+    i = 1;
+  }
+  while (_elements[i - 1] < value && i < _max_elements) {
+    _elements.push_back(i*(i + 1)/2);
+    ++i;
+  }
+  if (i == _max_elements) {
+    cerr << "Triangular Sequence: value too large " << value << " exceeds max"
+    << " size of " << _max_elements << endl;
+  }
+}
--- Chapter4/Exercises/Triangular.hpp
+++ Chapter4/Exercises/Triangular.hpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 class Triangular {
@@ -9,23 +11,35 @@ public:
   Triangular(int length, int beginning_position);
   Triangular(const Triangular &rhs);
 
+  // Static member functions
+  static bool is_element(int element);
+  static void generate_elements(int length);
+  static void generate_elements_to_value(int value);
+  static void display(int length, int beginning_position, ostream &os = cout);
+
   // Constant member functions
   int length() const {
     return _length;
   }
   int begin() const {
-    return _ beginning_position;
+    return _beginning_position;
   }
   int element(int position) const;
 
-  // Non-constant member functions
-  bool next(int &val);
-  void next_reset() {
+  bool next(int &val) const;
+  void next_reset() const {
     _next = _beginning_position - 1;
   }
+
+  Triangular& copy(const Triangular &rhs);
+
 private:
-  string _name;
+  string _name; // name of specific instantiation
   int _length; // number of elements
   int _beginning_position; // beginning position of range
-  int _next; // next element to iteratore over
+  mutable int _next; // next element to iteratore over
+
+  // Static data members
+  static const int _max_elements = 1024;
+  static vector<int> _elements;
 };
