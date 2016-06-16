23113105162762ebed150be7937d1d1ba9d3a742
Hawkins, Dakota Y <dakota.hawkins@pnnl.gov>
2016-06-16 10:03:19 -0700

    Renamed TriangularIterator* to Triangular_Iterator.

---
+++ Chapter4/Exercises/Triangular_iterator.cpp
@@ -0,0 +1,21 @@
+#include "Triangular_iterator.hpp"
+
+void Triangular_iterator::check_integrity() const {
+  if (_index > Triangular::_max_elements) {
+    throw iterator_overflow();
+  }
+  if (_index > Triangular_iterator::_elements.size()) {
+    Triangular::generate_elements(_index);
+  }
+}
+
+inline int& Triangular_iterator::operator ++() {
+  ++_index;
+  check_integrity();
+  return Triangular::_elements[_index];
+}
+
+inline int Triangular_iterator::operator ++(int) {
+  check_integrity()
+  return Triangular::_elements[_index++]; 
+}
---
+++ Chapter4/Exercises/Triangular_iterator.hpp
@@ -0,0 +1,24 @@
+#include "Triangular.hpp"
+using namespace std; 
+
+class Triangular_iterator {
+public:
+  Triangular_iterator(int index) : _index(index - 1){}
+  //inline bool operator == (const Triangular_iterator&) const;
+  inline bool operator == (const Triangular_iterator &rhs) const {
+    return _index == rhs._index;
+  }
+  //bool operator != (const Triangular_iterator&) const;
+  inline bool operator != (const Triangular_iterator &rhs) const {
+    return !(*this == rhs);
+  }
+  inline int operator * (const Triangular_iterator &rhs) const {
+    rhs.check_integrity();
+    return Triangular::_elements[_index];
+  }
+  int& operator ++ (); // prefix version
+  int operator ++ (int); // postfix version
+private:
+  void check_integrity() const;
+  int _index;
+};
